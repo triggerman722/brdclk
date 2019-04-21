809f1b0eb92f730ce02e4a58991f59b50308dcf0
Triggerman <gemartin@gmail.com>
2019-04-20 21:15:30 -0400

    new

---
+++ boardclick/boards/deleteboard.php
@@ -0,0 +1,17 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] === 'DELETE' || (isset($_REQUEST['_method']) && $_REQUEST['_method'] === 'delete')) {
+        $directory = getcwd();
+        delTree($directory);
+        header("Location:/");
+    }
+
+function delTree($dir) { 
+   $files = array_diff(scandir($dir), array('.','..')); 
+    foreach ($files as $file) { 
+      (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+    } 
+    return rmdir($dir); 
+} 
+
+
+?>
--- boardclick/boards/viewboard.php
+++ boardclick/boards/viewboard.php
@@ -1,19 +1,9 @@
 <?php
-require_once("../../session_mgmt.php");
-$board = json_decode(file_get_contents('board.json'), true);
-$directors = json_decode(file_get_contents('directors.json'), true);
-if ($board['isprivate']===true) {
-    if (!isDirector($directors, $username)) {
-        header("Location:/login");
-        die();
-    }
-}
-$title="Welcome to the ".$board['name']." home!";
 $body=<<<EOT
  <div class="row">
  <div class="col-3">
   <div class="card">
-   <img class="card-img-top" src="{$board['name']}">
+   <img class="card-img-top" src="{}">
    <div class="card-body">
     <img src="{}" class="rounded-circle" style="margin-top:-50px;width:64px;">
     <h5 class="card-title">{$board['name']}</h5>
@@ -31,18 +21,16 @@ $body=<<<EOT
     <span aria-hidden="true">&times;</span>
   </button>
    </div>
+<form method="POST">
+<input type="hidden" name="_method" value="delete" >
+<input type="submit" class="btn btn-danger" value="Delete">
+</form>
  </div>
  </div>
+
 EOT;
 include("../../base.php");
 
-function isDirector($directors, $username) {
-   foreach ($directors as $key => $value) {
-       if ($value===$username) {
-           return true;
-       }
-   }
-   return false;
-}
+
 
 ?>
