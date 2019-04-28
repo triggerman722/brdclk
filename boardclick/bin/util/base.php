64245770569db7a20a0ac288f045796efc35a598
triggerman <gemartin@gmail.com>
2019-04-27 21:06:17 -0400

    hh

--- boardclick/bin/util/base.php
+++ boardclick/bin/util/base.php
@@ -14,7 +14,7 @@
     <title><?php echo $title; ?></title>
   </head>
   <body>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="/">Boardkit</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- boardclick/index.php
+++ boardclick/index.php
@@ -1,11 +1,11 @@
 <?php
 $title="Join";
 $prebody=<<<EOT
-<div class="col-md-12" style="background-image: url(/img/m1.jpg); background-size: 100% auto;">
+<div class="col-md-12" style="background-image: url(/img/m1.jpg); background-size: cover;background-position:center;">
 <div class="row pt-5 pb-3" style="background-color:#00000080;">
     <div class="container py-5 text-center">
       <h1 class="display-2 text-light">Board Rocket Fuel</h1>
-      <h3 class="text-light">Meaningful collaboration at the center of your Board operations.</h3>
+      <h3 class="text-light">Boardkit enables meaningful collaboration in all of your Board operations.</h3>
       <p class="pt-2"><a class="btn btn-primary btn-lg" href="#" role="button">Start Your FREE Trial »</a></p>
     </div>
   </div>
@@ -15,7 +15,7 @@ $body=<<<EOT
 
 <div class="row mt-5">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+        <h2 class="featurette-heading">Setup takes seconds. <span class="text-muted">Nothing to download, nothing to install.</span></h2>
         <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-5">
@@ -25,7 +25,7 @@ $body=<<<EOT
 <hr class="my-5">
 <div class="row">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <h2 class="featurette-heading">Digitally sign documents. <span class="text-muted">To securely approve consent.</span></h2>
         <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
       </div>
       <div class="col-md-5 order-md-1">
@@ -35,8 +35,8 @@ $body=<<<EOT
 <hr class="my-5">
 <div class="row">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Chat-centric design. <span class="text-muted">For deep collaboration.</span></h2>
+        <p class="lead">We've made chat capability a core feature of Boardkit. At every point in your journey, your voice will be heard.</p>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
